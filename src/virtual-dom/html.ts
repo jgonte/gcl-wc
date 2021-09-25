203f1f8ad12ae532662cb3bcdc1d1caf7c7e2d2c
jgonte <jgonte@gmail.com>
2021-09-25 12:05:53 -0400

    Saved work

--- src/virtual-dom/html.ts
+++ src/virtual-dom/html.ts
@@ -7,30 +7,70 @@ import { EMPTY_STRING } from "../utils/shared";
  */
 export default function html(strings: TemplateStringsArray, ...values: any): VirtualNode | VirtualNodePart | string | null {
 
-    // const preMarkup = values.reduce(
-    //     (acc, val, idx) => [...acc, `<!--{{${idx}}}-->`, strings[idx + 1]],
-    //     [strings[0]]
-    // ).join('');
-
-    // console.log(preMarkup);
+    const parts = [];
 
     const markup = values.reduce(
-        (acc, val, idx) => [...acc, stringify(val), strings[idx + 1]],
+        (acc, val, idx) => [...acc, stringify(val, parts), strings[idx + 1]],
         [strings[0]]
     ).join('');
 
-    return markupToVirtualNode(markup, 'html', { excludeTextWithWhiteSpacesOnly: true });
+    const vnode = markupToVirtualNode(markup, 'html', { excludeTextWithWhiteSpacesOnly: true });
+
+    if (parts.length > 0) {
+
+        let node = (vnode as VirtualNode).$node;
+
+        if (node === undefined) {
+
+            node = new DocumentFragment();
+        }
+
+        const comments = Array.from(node.childNodes)
+            .filter(n => n.nodeType === Node.COMMENT_NODE);
+
+        parts.forEach((part, i) => {
+
+            // Add as a child of the vnode
+            (vnode as VirtualNode).children.push(part);
+
+            const comment = comments.length > i ?
+                comments[i] :
+                undefined;
+
+            if (comment !== undefined) { // Prepend its DOM node to the next comment placeholder of the dom node
+
+                node.insertBefore(part.$node, comment);
+            }
+            else { // Add the part and the marker
+
+                node.appendChild(part.$node);
+
+                node.appendChild(new Comment());
+            }
+        });
+    }
+
+    return vnode;
 }
 
-function stringify(value) {
+function stringify(value, parts) {
 
+    if (Array.isArray(value) && isVirtualNode(value[0])) { // Recurse for every item of the array
+
+        return value.reduce((acc, val, i) => acc = [...acc, stringify(val, parts)], [])
+            .join('')
+    }
     if (isVirtualNode(value)) { // Handle virtual nodes
 
-        return createMarkup(value as VirtualNode);
+        parts.push(value);
+
+        return '<!---->';
+
+        //return createMarkup(value as VirtualNode);
     }
     else if (typeof value === 'function') {
 
-        return stringify(value());
+        return stringify(value(), parts);
     }
     else if (typeof value === 'object') {
 
@@ -105,4 +145,5 @@ function renderChildren(vnode: VirtualNode) {
         []
     ).join('');
 }
+
 
